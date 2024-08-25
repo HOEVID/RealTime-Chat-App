81eee83cfb27915fd91883a506d4c703efd0ac53
HOEVID <shreyanshop22@gmail.com>
2024-08-25 15:44:11 +0530

    Setting up firebase and databse

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import Chat from './pages/chat/Chat'
 import Login from './pages/login/Login'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 import { Routes,Route } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   return (
     <>
+    <ToastContainer/>
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/chat' element={<Chat/>}/>
---
+++ src/config/firebase.js
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABkP99PPyjyISzpdn6gb0CTwF2YWaAHtg",
+  authDomain: "real-time-chat-app-56995.firebaseapp.com",
+  projectId: "real-time-chat-app-56995",
+  storageBucket: "real-time-chat-app-56995.appspot.com",
+  messagingSenderId: "367812567780",
+  appId: "1:367812567780:web:a75e1da5ed7b947e3282f1",
+  measurementId: "G-3N1Y20SZSW"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth =getAuth(app)
+const db=getFirestore(app)
+const storage = getStorage(db)
+
+const signup = async(username,email,password)=>{
+try
+   {const res =await createUserWithEmailAndPassword(auth,email,password)
+    const user= res.user;
+    await setDoc(doc(db,"users",user.uid),{
+        id:user.uid,
+        username:username.toLowerCase(),
+        email,
+        name:"",
+        avatar:"",
+        bio:"Hey,I am using Chat App",
+        lastseen:Date.now()
+    })
+    await setDoc(doc(db,"chats",user.uid),{
+        chatData:[]
+    })
+   }
+   catch(err){
+    console.err(err);
+   }
+}
