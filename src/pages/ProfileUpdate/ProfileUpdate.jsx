a9b639ff2c47d165bd584704308b62b6d00a99ba
HOEVID <shreyanshop22@gmail.com>
2024-08-25 15:27:04 +0530

    Completed profileUpdate page

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 const ProfileUpdate = () => {
+    const [image,setImage]=useState(false)
   return (
-    <div>Profile Page</div>
+    <div className='profile'>
+        <div className="profile-container">
+       
+            <form>
+            <img className="logo-icon"src={ image?URL.createObjectURL(image):assets.logo_icon}  alt=""/>
+                <h2>Profile details</h2>
+          
+           
+             <label htmlFor='image'>
+             <input onChange={(e)=>setImage(e.target.files[0])}type="file" placeholder='Upload image' accept='/image/png, /image/jpg' id='image' hidden /> 
+                <img src={image? URL.createObjectURL(image) :assets.avatar_icon} alt="" />
+                Upload Image here
+             </label>
+            
+            
+             <input type='text' placeholder='Your name' id="name" required />
+             <textarea placeholder='Wrie your bio' id="bio " required>
+             </textarea>
+            <button >Save</button>
+        </form>
+        
+        </div>
+    </div>
   )
 }
 
